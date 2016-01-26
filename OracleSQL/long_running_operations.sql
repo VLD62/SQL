f988fa7581af6ba3f8fd768b742fe12c3b240dfd
VLD62 <iliev62@gmail.com>
2016-01-26 13:25:38 +0200

    Create long_running_operations.sql

---
+++ OracleSQL/long_running_operations.sql
@@ -0,0 +1,10 @@
+SELECT osuser,
+       sl.sql_id,
+       sl.sql_hash_value,
+       opname,
+       target,
+       elapsed_seconds,
+       time_remaining
+  FROM v$session_longops sl
+inner join v$session s ON sl.SID = s.SID AND sl.SERIAL# = s.SERIAL#
+WHERE time_remaining > 0;
