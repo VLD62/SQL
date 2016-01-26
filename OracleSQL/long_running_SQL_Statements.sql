e2d1e6a92c65ad63d228b944c08a092468878ce3
VLD62 <iliev62@gmail.com>
2016-01-26 13:26:27 +0200

    Create long_running_SQL_Statements.sql

---
+++ OracleSQL/long_running_SQL_Statements.sql
@@ -0,0 +1,15 @@
+SELECT s.username,
+       sl.sid,
+       sq.executions,
+       sl.last_update_time,
+       sl.sql_id,
+       sl.sql_hash_value,
+       opname,
+       target,
+       elapsed_seconds,
+       time_remaining,
+       sq.sql_fulltext
+  FROM v$session_longops sl
+ INNER JOIN v$sql sq ON sq.sql_id = sl.sql_id
+ INNER JOIN v$session s ON sl.SID = s.SID AND sl.serial# = s.serial#
+ WHERE time_remaining > 0;
